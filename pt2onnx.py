053aee267075e77bb4592169d20f3ed0570a6d53
Wys <56908836+wangyansenhub@users.noreply.github.com>
2024-12-05 17:43:26 +0800

    Create pt2onnx.py

---
+++ pt2onnx.py
@@ -0,0 +1,5 @@
+# yolov5:
+# python export.py --weights ./yolov5l.pt --opset 10 --include onnx
+
+# yolov8：
+# yolo export model=D:\gitlab\weights\v8\seg_weights\yolov8n-seg.pt format=onnx opset=10 imgsz=640
