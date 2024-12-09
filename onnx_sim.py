eb652587503a0215679cd2da02d0ede7e5695e33
Wys <56908836+wangyansenhub@users.noreply.github.com>
2024-12-09 17:35:00 +0800

    Update onnx_sim.py

--- onnx_sim.py
+++ onnx_sim.py
@@ -17,3 +17,11 @@ if __name__ == '__main__':
     assert check, "Simplified ONNX model could not be validated"
     onnx.save(model_simp, onnx_file)
     print('finished exporting onnx')
+
+
+
+# yolov5:
+# python export.py --weights ./yolov5l.pt --opset 10 --include onnx
+
+# yolov8：
+# yolo export model=D:\gitlab\weights\v8\seg_weights\yolov8n-seg.pt format=onnx opset=10 imgsz=640 
